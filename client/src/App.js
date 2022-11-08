32e2c122d0d8c5483e2109dafe8027b7c55414dd
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-08 13:41:38 -0500

    debugg

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Dice from './component/Dice';
 import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Dice />
     </div>
   );
 }
--- client/src/component/Dice.jsx
+++ client/src/component/Dice.jsx
@@ -1,30 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 const Dice = () => {
-  recipesArr = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']
+  const recipesArr = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13'];
+
   const [roll, setRoll] = useState(false)
 
   const submitHandler = () => {
-    if (roll) {
-      setRoll(false);
-    }
     setRoll(true);
   }
 
   const randonEffect = () => {
-    recipesArr.foreach((item) => {
-      <span>item</span>
-    }
+    recipesArr.foreach(item =>
+      <span>{item}</span>
     )
+    setRoll(false);
   }
-
 
 
   return (
     <div>
-      <span> {roll ? randonEffect : 'something'}</span>
+      {recipesArr.map(item =>
+        <span>{item}</span>
+      )}
       <button type="Submit" onClick={submitHandler}>Roll</button>
     </div>
 
   )
 }
+
+export default Dice;
