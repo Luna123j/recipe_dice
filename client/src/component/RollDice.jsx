d69e380f2baa1ac0b26d64b6bbaeae50298d78ba
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-14 15:43:33 -0500

    fix the issue in setinteval

--- client/src/component/RollDice.jsx
+++ client/src/component/RollDice.jsx
@@ -7,28 +7,38 @@ import './RollDice.scss'
 
 const RollDice = (props) => {
   const recipesArr = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13'];
-
   const [state, setState] = useState({
     randomRecipe: 'none',
-    rolling: false
+    rolling: false,
   })
 
+  const [stop, setStop] = useState(false)
+
+
   const roll = () => {
+    // const recipesIndex = Math.floor(Math.random() * recipesArr.length);
     let timesRepeat = 0;
 
     const repeat = setInterval(() => {
       timesRepeat += 1;
       setState({
         randomRecipe: recipesArr[Math.floor(Math.random() * recipesArr.length)],
-        rolling: true
+        rolling: true,
       })
 
       if (timesRepeat === 20) {
-        setState({ ...state, rolling: false });
+        setStop(true)
         clearInterval(repeat);
       }
     }, 50)
   }
+
+  useEffect(() => {
+    if (state.rolling && stop) {
+      setStop(false)
+      setState({ ...state, rolling: false });
+    }
+  }, [state])
 
   return (
     <div className="rollDice">
