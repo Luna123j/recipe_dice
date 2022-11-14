44dc58f45b05f117a45da68ce094691f4ac091d0
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-14 15:26:03 -0500

    delete un-useful code and make the random effect works

--- client/src/component/Dice.jsx
+++ client/src/component/Dice.jsx
@@ -6,7 +6,6 @@ const Dice = (props) => {
 
   return (
     <div className="dice">
-      <i className = {`fa-solid fa-dice-${props.side}`} ></i>
       <p>{props.randomRecipe}</p>
     </div>
 
--- client/src/component/RollDice.jsx
+++ client/src/component/RollDice.jsx
@@ -7,35 +7,33 @@ import './RollDice.scss'
 
 const RollDice = (props) => {
   const recipesArr = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13'];
-  const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
 
   const [state, setState] = useState({
-    die: 'one',
     randomRecipe: 'none',
     rolling: false
   })
 
   const roll = () => {
-    setState({
-      die: sides[Math.floor(Math.random() * sides.length)],
-      randomRecipe: recipesArr[Math.floor(Math.random() * recipesArr.length)],
-      rolling: true
-    })
+    let timesRepeat = 0;
+
+    const repeat = setInterval(() => {
+      timesRepeat += 1;
+      setState({
+        randomRecipe: recipesArr[Math.floor(Math.random() * recipesArr.length)],
+        rolling: true
+      })
+
+      if (timesRepeat === 20) {
+        setState({ ...state, rolling: false });
+        clearInterval(repeat);
+      }
+    }, 50)
   }
-
-  useEffect(() => {
-    if (state.rolling) {
-      setTimeout(() => {
-        setState({ ...state, rolling: false })
-      }, 1000)
-    }
-  }, [state])
-
 
   return (
     <div className="rollDice">
       <div className="diceContainer">
-        <Dice side={state.die} rolling={state.rolling} randomRecipe ={state.randomRecipe}  />
+        <Dice rolling={state.rolling} randomRecipe={state.randomRecipe} />
         <button type="Submit" onClick={roll}> {state.rolling ? 'Rolling' : 'Click to Roll'} </button>
       </div>
     </div>
