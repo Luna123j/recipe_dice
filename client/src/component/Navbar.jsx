b212f0e9fd7300eee9d000f931cafdeddb0d5308
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-14 21:26:18 -0500

    edit language button

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import './Navbar.scss';
 const Navbar = () => {
+
+  const [lan, setLan] = useState('English')
+
+  const handleSelect = (e) => {
+    setLan(e.target.innerText)
+  }
+
+
   return (
     <nav className="navbar navbar-default">
       <div className="navbarItems">
@@ -12,13 +21,19 @@ const Navbar = () => {
             Recipes
           </li>
         </ul>
-        <div className="nav dropdown">
-          <button className="dropdown-btn btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown">E/C</button>
-          <div className="dropdown-menu">
-            <p className="dropdown-item ">EN</p>
-            <p className="dropdown-item ">CN</p>
+        <div  className="right-coner">
+          <div className="nav dropdown">
+            <button className="dropdown-btn btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" ><i className="fa-solid fa-globe"></i></button>
+            <div className="dropdown-menu">
+              <p className="dropdown-item " name='English' onClick={handleSelect}>English {lan === 'English' ? <i className="fa-solid fa-check"></i> : ""}</p>
+              <p className="dropdown-item " name='中文' onClick={handleSelect}>中文 {lan === '中文' ? <i className="fa-solid fa-check"></i> : ""} </p>
+            </div>
+          </div>
+          <div className="nav-item">
+            login/register
           </div>
         </div>
+
       </div>
     </nav>
 
