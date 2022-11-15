b165370a627de04f6b6cef143d2ab1848f70a610
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-15 16:00:44 -0500

    Revert "add recipes component"
    This reverts commit b5451cd3dbae030240319a390f0bc9571221039a.

---
+++ client/src/component/EditDice.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './EditDice.scss';
+import { useNavigate } from "react-router-dom";
+
+const EditDice = (props) => {
+  let inputArr = [];
+  const navigate = useNavigate()
+  const inputHandler = (e) => {
+    e.preventDefault();
+    let inputStr = e.target.value;
+
+    if (inputStr.length > 0) {
+      inputArr = inputStr.split(',');
+    }
+  }
+
+  return (
+    <div className="editDiv">
+      <label>put content-----seperate items by ,</label>
+      <input id='input recipe' onChange={inputHandler} />
+
+      <button type="Submit" onClick={()=>{props.editContent(inputArr); navigate('/')}}>update</button>
+    </div>
+  )
+}
+
+export default EditDice;
