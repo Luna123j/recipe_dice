d74ef113641a8ca4391aa80034f9495f38bb266f
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-15 14:19:22 -0500

    set state for dice content

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import RollDice from './component/RollDice';
 import Navbar from './component/Navbar';
-import { BrowserRouter , Switch, Route, Link, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link, Routes, useNavigate } from "react-router-dom";
 import EditDice from './component/EditDice';
+import React, { createContext, useContext, useState } from 'react';
+
 
 
 function App() {
+
+  const [diceContent, setDiceContent] = useState(['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13'])
+ 
+  const handleEdit = (arr)=>{
+    setDiceContent(arr);
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<RollDice />} />
-        <Route path="/editDice" element={<EditDice />} />
+          <Route path="/" element={<RollDice diceContent = {diceContent}/>} />
+          <Route path="/editDice" element={<EditDice editContent = {handleEdit}/>} />
       </Routes>
     </div>
   );
--- client/src/component/EditDice.jsx
+++ client/src/component/EditDice.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-const EditDice = ()=>{
+const EditDice = (props)=>{
+
+  const inputHandler = (e)=>{
+    e.preventDefault();
+    let inputStr = e.target.value;
+    let inputArr = []
+    if (inputStr.length>0){
+      inputArr = inputStr.split(',');
+    }
+  }
 
   return (
-    <div>
-      <label>put content</label>
-      <input />
+    <div className="editDiv">
+      <label>put content-----seperate items by ,</label>
+      <input id = 'input recipe' onChange={inputHandler}/>
+      <button type="Submit">update</button>
     </div>
   )
 }
