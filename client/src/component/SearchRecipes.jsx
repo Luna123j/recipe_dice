39233e634a4d1f3dac797e7a801e252b0ef3f810
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-15 20:48:38 -0500

    display results

--- client/src/component/SearchRecipes.jsx
+++ client/src/component/SearchRecipes.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-const SearchRecipes = (props)=>{
+const SearchRecipes = (props) => {
   console.log(props.apiResult)
+  const apiResult = props.apiResult;
+
   return (
-    <div>Recipes result</div>
-                                          
+    <div>
+      <table className="table">
+        <tbody>
+          {apiResult.map((obj,index) => {
+            let content = obj.recipe;
+            return(
+             <tr key={index}>
+              <td ><img src={`${content.images.THUMBNAIL.url}`} alt={`${content.label}`}/></td>
+              <td>{content.label}</td>
+            </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+
   )
 }
 
