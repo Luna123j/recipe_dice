ff1fdb3d387b197b04b474af1368a31f2b3742b4
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-21 21:58:47 -0500

    add more styling for edit page

--- client/src/component/EditDice.jsx
+++ client/src/component/EditDice.jsx
@@ -5,6 +5,15 @@ import { useNavigate } from "react-router-dom";
 const EditDice = (props) => {
   let inputArr = [];
   const navigate = useNavigate()
+  const fomatContent = props.diceContent.map((item, index) => {
+    if (index !== 0) {
+      return " " + item
+    }else{
+      return item
+    }
+
+  })
+
   const inputHandler = (e) => {
     e.preventDefault();
     let inputStr = e.target.value;
@@ -14,12 +23,12 @@ const EditDice = (props) => {
     }
   }
 
+
   return (
     <div className="editDiv">
       <label><mark>Modify contents ----- Seperate items by <strong>COMMA</strong></mark></label>
-      <textarea id='inputRecipe' defaultValue={props.diceContent} onChange={inputHandler} />
-
-      <button className="updateBtn" type="Submit" onClick={()=>{props.editContent(inputArr); navigate('/')}}>update</button>
+      <textarea cols="100" rows="30" id='inputRecipe' defaultValue={fomatContent} onChange={inputHandler} />
+      <button className="updateBtn" type="Submit" onClick={() => { props.editContent(inputArr); navigate('/') }}>update</button>
     </div>
   )
 }
