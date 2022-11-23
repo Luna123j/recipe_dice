abcc269ee9337b7d493472d4a186ac31413b833f
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-22 21:14:53 -0500

    styling recipe detal page

--- client/src/component/RecipeDetail.jsx
+++ client/src/component/RecipeDetail.jsx
@@ -3,25 +3,37 @@ import './RecipeDetail.scss'
 
 const RecipeDetail = (props) => {
   const details = props.detail;
-  
+
   return (
     <div className="detailContainer">
-      <p>{details.label}</p>
-      <img src={details.images.SMALL.url}></img>
-      <tbody>{details.ingredients.map((line,index) => {
-        return (
-          <tr key={index}>
-            <td>{line.food}</td>
-            <td>{line.quantity}       {line.measure}</td>
-          </tr>
-        )
-      })}
-      </tbody>
-      <div>{details.ingredientLines.map((line,index)=>{
-        return(
-          <p key={index}>{line}</p>
-        )
-      })}</div>
+      <section className="section1">
+        <img src={details.images.REGULAR.url}></img>
+        <p className="name">{details.label}</p>
+        <p className="tags">{details.mealType} | {details.cuisineType} | {Math.floor(details.calories)} cal.</p>
+      </section>
+      <div className="section2">
+        <p className="time">Cooking time: {details.totalTime} min</p>
+        Ingredients
+        <table >
+        <tbody>{details.ingredients.map((line, index) => {
+          return (
+            <tr key={index}>
+              <td>{line.food}</td>
+              <td>{Math.floor(line.quantity * 100) / 100}       {line.measure}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+        <p className="steps">Steps</p>
+        <div className="steps"> {details.ingredientLines.map((line, index) => {
+          return (
+            <p key={index}>step {index+1} <br /><p className="lighter">{line}</p> </p>
+          )
+        })}</div>
+      </div>
+      
+
     </div>
 
   )
